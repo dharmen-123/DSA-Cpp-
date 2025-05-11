d408279eff53a45d86f8f235df8ca9f2f1f4db26
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-11 22:42:15 +0530

    sorting

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -3,10 +3,12 @@ using namespace std;
 void bubble(int arr[] , int s){
  
     for(int i=0; i<s ; i++){
-     for(int j=0; j<s ; j++){
+     for(int j=0; j<s-1 ; j++){
+        if(arr[j]>arr[j+1]){
         int temp=arr[j];
         arr[j]=arr[j+1];
         arr[j+1]=temp;
+        }
     }   
     }
 }
