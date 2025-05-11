7786aa3bbc573c5a09db9a2dcc879520ccec88ad
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-11 22:46:08 +0530

    commit

--- Bubblesort.cpp
+++ Bubblesort.cpp
@@ -1,9 +1,11 @@
-#include"iostream"
+           ///////   BUBBLE SORTING 
+
+           #include"iostream"
 using namespace std;
 void bubble(int arr[] , int s){
  
     for(int i=0; i<s ; i++){
-     for(int j=0; j<s ; j++){
+     for(int j=0; j<s-1 ; j++){
         if(arr[j]>arr[j+1]){
         int temp=arr[j];
         arr[j]=arr[j+1];
@@ -12,7 +14,6 @@ void bubble(int arr[] , int s){
     }   
     }
 }
-
 int main(){
    int arr[]={7,3,9,6,2};
    int n=sizeof(arr)/sizeof(arr[0]);
