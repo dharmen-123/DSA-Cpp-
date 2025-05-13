c78b3f61c9da0e62819a6f31a834c3543c508a63
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-13 21:04:46 +0530

    optimisation

--- OptimiseBubble.cpp
+++ OptimiseBubble.cpp
@@ -1,9 +1,10 @@
 #include"iostream"
 using namespace std;
 void bubble(int arr[], int s){
-int temp;
+int temp,c=0;
   for(int i=0;i<s;i++){
     for(int j=0;j<s-i-1;j++){
+      c++;
         if(arr[j]>arr[j+1]){
          temp=arr[j];
          arr[j]=arr[j+1];
@@ -11,19 +12,20 @@ int temp;
         }
     }
   }
+  cout<<"Loop is runing "<<c<<" Times \n";
 }
 
 int main(){
     int s;
-    int arr[s];
     cout<<"Enter the array size \n";
     cin>>s;
-    cout<<"Enter "<<s<<" values";
+    int arr[s];
+    cout<<"Enter "<<s<<"Values \n";
     for(int i=0;i<s;i++){
         cin>>arr[i];
     }
     bubble(arr, s);
     for(int i=0;i<s;i++){
-        cout<<arr[i];
+        cout<<arr[i]<<"\t";
     }
 }
