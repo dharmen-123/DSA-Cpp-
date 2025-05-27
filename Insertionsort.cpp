b025d4888fd419b514c07a798c95417af2f8c214
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-27 16:31:40 +0530

    commit

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -1,4 +1,15 @@
 //// ###### INSERTION SORT ########## ////
 // It is Online Algorithm //
- 
+
+#include"iostream"
+using namespace std;
+void Insertion(int arr[], int n){
+
+
+}
+    
+int main(){
+    int arr[]={8,9,6,5,1};
+    
+}
 
