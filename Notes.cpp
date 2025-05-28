b92635194377f53839b059a2bd656c4d7ac17dd6
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-28 22:39:37 +0530

    changes

---
+++ Notes.cpp
@@ -0,0 +1,13 @@
+// Stack and heap is memory
+// Constant value are negligible for complexity
+
+
+
+  // Insertion Sort is Stable sorting algorithm  
+//  It is also Inplace 
+// Time Complexity O(n)
+// Space Complexity is O(n) 
+
+// here while loop is used instead of if because if check only once at a time
+// Why minimum value is greater than zero index
+// because it start from one index
