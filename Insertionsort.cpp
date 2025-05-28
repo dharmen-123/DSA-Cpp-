da30a0315cbccaee2cc8d5a88cdd2584867c3806
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-28 16:33:34 +0530

    update

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -4,29 +4,41 @@
 #include"iostream"
 using namespace std;
 void Insertion(int arr[], int s){
+  int count=0;
     for(int i=1;i<s;i++){
       int min=i;
       while(min>0 && arr[min]<arr[min-1]){
+        count++;
          int tem=arr[min-1];
          arr[min-1]=arr[min];
          arr[min]=tem;
          min--;
         }
     }
+    cout<<"No of hits "<<count;
 }
 int main(){
-    int arr[]={8,9,6,5,1};
-    int s= sizeof(arr)/sizeof(arr[0]);
-   for(int i=0;i<s;i++){
-     cout<<arr[i]<<" ";
-   }
+    int s;
+    cout<<"Enter the array size \n";
+    cin>>s;
+    int arr[s];
+    cout<<"Enter "<<s<<"Values \n";
+    for(int i=0;i<s;i++){
+        cin>>arr[i];
+    }
     Insertion(arr, s);
-     cout<<"\nafter\n";
+     cout<<"\n after Sorting \n";
   for(int i=0;i<s;i++){
      cout<<arr[i]<<" ";
    }
   }
 
+  // Insertion Sort is Stable sorting algorithm  
+//  It is also Inplace 
+// Time Complexity O(n)
+// Space Complexity is O(n) 
 
+
+// here while loop is used instead of if because if check only once at a time
 // Why minimum value is greater than zero index
 // because it start from one index
