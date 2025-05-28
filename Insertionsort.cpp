8aae9a1941804416ca819d7367ec0ba412caa77f
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-28 15:50:50 +0530

    changes

--- Insertionsort.cpp
+++ Insertionsort.cpp
@@ -4,12 +4,17 @@
 #include"iostream"
 using namespace std;
 void Insertion(int arr[], int n){
-
-
+   
+    
+ 
 }
     
 int main(){
     int arr[]={8,9,6,5,1};
-    
+    int s= sizeof(arr)/sizeof(arr[0]);
+   for(int i=0;i<s;i++){
+     cout<<arr[i]<<" ";
+   }
+    Insertion(arr, s);
 }
 
