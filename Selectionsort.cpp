b37ebad9095c1b425848b68ba20ca642aeaefd30
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-28 22:37:56 +0530

    selection sort

--- Selectionsort.cpp
+++ Selectionsort.cpp
@@ -5,8 +5,8 @@ void Selection(int arr[], int s)
     int hit=0;
     for (int i = 0; i < s - 1; i++)      // loop is run (n-1) times
     {  
-        hit++;
         int min = i;
+        hit++;
         for (int j = i + 1; j < s; j++)     // loop is running n times
         {
             if (arr[j]< arr[min])
