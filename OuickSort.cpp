b3f2ca820559a4d8fc9c54e3eacce2cf57e08fc0
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-30 16:23:48 +0530

    changes

--- OuickSort.cpp
+++ OuickSort.cpp
@@ -3,3 +3,17 @@
 // It follow Divide and conquer approach 
 // It use pivot element 
 // It divide array in two part may be or not
+// It also use Recurssion 
+
+// LOGIC of Quick Sort 
+//choose first element as a pivot 
+// then after pivot element first element as P and last element is Q
+// if P > Pivot then
+//    stop
+// else
+//    P++
+// and
+// if Q < Pivot then 
+//    stop
+// else
+//    Q--
