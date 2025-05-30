61078b73509394c6f3062eb67328e2291317455f
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-05-30 15:11:43 +0530

    commit

--- SELECTIONsorting.cpp
+++ SELECTIONsorting.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 using namespace std;
 int main(){
+
     int arr[]={3,14,2,5,7};
     int s=sizeof(arr)/sizeof(arr[0]);
     for(int i=0;i<s;i++){
         cout<<arr[i]<<"\t";
+    
     }
     for(int i=0;i<s;i++){
         int min=i;
