778b73be3fa8074c59e070c9f3e58903a379f54e
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-01 20:10:25 +0530

    commit

---
+++ Practice.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+void bubble(int arr[],int s)
+{
+    int temp,c=0;
+    bool d;
+    for (int i=0;i<s;i++)
+    {
+        d=false;
+        for (int j=0;j<s-i-1;j++)
+        {
+            c++;
+            if(arr[j]>arr[j+1])
+            {
+                temp=arr[j+1];
+                arr[j+1]=arr[j];
+                arr[j]=temp;
+                d=true;
+            }        
+        } 
+        if (d==false){
+            break;
+        }
+    }
+    cout<<"counter hit "<<c<<endl;
+}
+int main()
+{
+    int arr[6]={6,5,4,3,2,1};
+    bubble (arr,6);
+    for(int i=0;i<6;i++)
+    {
+        cout<<arr[i]<<endl;
+    }
+}
