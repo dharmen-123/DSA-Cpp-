bea0f8be6f53fe6985221f4a8dacf399d339d0a5
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-02 16:07:25 +0530

    recursion

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,3 +1,10 @@
 // ######## RECURSION ######## 
 // It is define as the function calling it self 
 // It has two parameter 1 base condition 2 recursion relationship
+
+// every time recursion program will be create parameterized function   
+
+// Type of recursion 
+// Head Recursion and Tail Recursion 
+//  If function call before print in recursion it is called Tail recursion
+//  If function call After print in recursion it is called Head recursion
