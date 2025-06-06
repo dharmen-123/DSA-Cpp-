35d3f3c02a52f3eae9e9fdc77b9c1a5687ee38fe
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-06 15:53:46 +0530

    changes

---
+++ MergeSort.cpp
@@ -0,0 +1,7 @@
+// // ######### MERGE SORT ##############
+// 1 Mid Value 
+// 2 Partition 
+// 3 Divide and conquor
+// 4 Merge
+// 5 Time Complexity O(nlogn)
+// in All Three cases 
--- Notes.cpp
+++ Notes.cpp
@@ -20,3 +20,43 @@
 //Worst Case : O(n)
 
 // Bubble , Selection and Insertion are the time taken algorithms
+
+////  QUICK SORTING 
+// It is mostly useful sorting algorithm 
+// It follow Divide and conquer approach 
+// It use pivot element 
+// It divide array in two part may be or not
+// It also use Recurssion 
+//It is not mandatory to select the pivot element is always first element  
+// It use two pointer
+
+// ######## RECURSION ######## 
+// It is define as the function calling it self 
+// It has two parameter 1 base condition 2 recursion relationship
+
+// every time recursion program will be create parameterized function   
+
+// Type of recursion 
+// Head Recursion and Tail Recursion 
+//  If function call before print in recursion it is called Tail recursion
+//  If function call After print in recursion it is called Head recursion
+
+
+////   LOGIC of Quick Sort 
+
+//choose first element as a pivot 
+// then after pivot element first element as P and last element is Q
+// if P > Pivot then
+//    stop
+// else
+//    P++
+// and
+// if Q < Pivot then 
+//    stop
+// else
+//    Q--
+
+
+// If two pointer are not cross each other then swap is done between two pointer 
+// and  if two pointer are cross each other then swap is done between 
+// last pointer Q  and pivot element and mid value is obtained
