1ba84717b8c9a37b0bb2f40b0cefedfccf342716
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-08 16:15:01 +0530

    insertion sort

--- Practice.cpp
+++ Practice.cpp
@@ -74,22 +74,23 @@
 #include<iostream>
 using namespace std;
 void Insertion(int arr[], int s){
-  
+ int c=0; 
       for(int i=1;i<s;i++){
           int min=i;
 
           while(min>0 && arr[min]<arr[min-1]){
-               int swp = arr[min];
+            c++;
+            int swp = arr[min];
                arr[min]=arr[min-1];
                arr[min-1]=swp;
+               min--;
           }
-
-
       }
+      cout<<"no. hits :"<<c<<"\n";
     
 }
 int main(){
-    int arr[]={4,2,5,6,8};
+    int arr[]={3,4,1,2,5,6};
     int s=sizeof(arr)/sizeof(arr[2]);
     Insertion(arr, s);
     for(int i=0;i<s;i++){
