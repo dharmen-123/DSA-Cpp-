a0a0fea8be84b9e685067c1ae4be845604749a7e
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-08 16:29:41 +0530

    Insertion Algo

--- Practice.cpp
+++ Practice.cpp
@@ -71,28 +71,59 @@
 
 // ############### Insertion Sort #################### 
 
+// #include<iostream>
+// using namespace std;
+// void Insertion(int arr[], int s){
+//  int c=0; 
+//       for(int i=1;i<s;i++){
+//           int min=i;
+
+//           while(min>0 && arr[min]<arr[min-1]){
+//             c++;
+//             int swp = arr[min];
+//                arr[min]=arr[min-1];
+//                arr[min-1]=swp;
+//                min--;
+//           }
+//       }
+//       cout<<"no. hits :"<<c<<"\n";
+    
+// }
+// int main(){
+//     int arr[]={3,4,1,2,5,6};
+//     int s=sizeof(arr)/sizeof(arr[2]);
+//     Insertion(arr, s);
+//     for(int i=0;i<s;i++){
+//       cout<<arr[i]<<"\t";
+//     }
+// }
+
+// ################### Selection Sort ##################### 
+
 #include<iostream>
 using namespace std;
-void Insertion(int arr[], int s){
- int c=0; 
-      for(int i=1;i<s;i++){
-          int min=i;
+void Selection(int arr[], int s){
+  
+     for(int i=0;i<s-1; i++){
+      int min =i;   
+        for(int j=i+1;j<s;j++){
 
-          while(min>0 && arr[min]<arr[min-1]){
-            c++;
-            int swp = arr[min];
-               arr[min]=arr[min-1];
-               arr[min-1]=swp;
-               min--;
-          }
+           if(arr[j]<arr[min]){
+             
+                min=j;
+
+           }
       }
-      cout<<"no. hits :"<<c<<"\n";
-    
+       if(min!=i){
+            swap(arr[min], arr[i]);
+       }
+     }
 }
+
 int main(){
-    int arr[]={3,4,1,2,5,6};
+    int arr[]={5,4,3,2,1};
     int s=sizeof(arr)/sizeof(arr[2]);
-    Insertion(arr, s);
+    Selection(arr, s);
     for(int i=0;i<s;i++){
       cout<<arr[i]<<"\t";
     }
