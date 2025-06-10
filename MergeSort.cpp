fed6138f05a49d593878359764ad7dda695ec261
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-10 15:56:45 +0530

    commit

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -12,29 +12,41 @@
 #include<iostream>
 using namespace std;
 
-void Merges(int arr[], int low, int mid, int high){
- int b[high+1];
- int i=low;
- int j=mid+1;
- int k=mid;
+// void Merges(int arr[], int low, int mid, int high){
+//  int b[high+1];
+//  int i=low;
+//  int j=mid+1;
+//  int k=mid;
 
+
+// }
+
+// void MergeSort(int arr[], int low, int high){
+// int mid;
+// if(low<high){
+//     mid = (low+high)/2;
+//     MergeSort(arr, low ,mid);
+//     MergeSort(arr, mid+1 ,high);
+//     Merges(arr,low , mid , high);
+// }
+
+// }
+void Merge(int arr[], int low, int mid, int high){
 
 }
-
-void MergeSort(int arr[], int low, int high){
-int mid;
-if(low<high){
-    mid = (low+high)/2;
-    MergeSort(arr, low ,mid);
-    MergeSort(arr, mid+1 ,high);
-    Merges(arr,low , mid , high);
-}
-
+void MSort(int arr[], int low, int high){
+    int mid;
+    if(low<high){
+        mid=(low+high)/2;
+        MSort(arr, low, mid);
+        MSort(arr, mid+1, high);
+        Merge(arr, low ,mid , high);
+    }
 }
 int main(){
    int arr[]={9,3,4,1,5,6};
    int s=sizeof(arr)/sizeof(arr[0]);
-   MergeSort(arr, 0, s-1);
+   MSort(arr, 0, s-1);
    for(int i=0;i<s;i++){
     cout<<arr[i]<<" ";
    }
