126cd1acf11acf128e79dbfcaf50658a38247135
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-11 15:04:03 +0530

    commit

--- Practice.cpp
+++ Practice.cpp
@@ -217,3 +217,56 @@
 //       cout<<arr[i]<<"\t";
 //     }
 // }
+
+
+//  ################# merge Sort ##################### 
+#include<iostream>
+using namespace std;
+void MERGE(int arr[], int low , int mid, int high){
+    int b[high+1];
+    int i=low;
+    int j=mid+1;
+    int k=low;
+    while(i<=mid && j<=high){
+        if(arr[i]<arr[j]){
+            b[k]=arr[i];
+            i++;
+        }
+        else{
+            b[k]=arr[j];
+            j++;
+        }
+        k++;
+    }
+    while(i<=mid){
+        b[k]=arr[i];
+        i++;
+        k++;
+    }
+    while(j<=high){
+        b[k]=arr[j];
+        j++;
+        k++;
+    }
+    for(int i=low;i<=high;i++){
+        arr[i]=b[i];
+    }
+}
+void MSort(int arr[], int low , int high){
+    int mid;
+    if(low<high){
+        mid=(low+high)/2;
+        MSort(arr, low, mid);
+        MSort(arr, mid+1, high);
+        MERGE(arr, low , mid , high);
+    }
+}
+
+int main(){
+     int arr[]={4,8,2,6,7};
+    int s=sizeof(arr)/sizeof(arr[2]);
+    MSort(arr, 0, s-1);
+    for(int i=0;i<s;i++){
+      cout<<arr[i]<<"\t";
+    }
+}
