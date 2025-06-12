38e0be9e75665fec6a29c46455b44af551b6cbf5
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-12 22:31:57 +0530

    commit

--- Tree.cpp
+++ Tree.cpp
@@ -2,3 +2,11 @@
 // Tree is defined as the collection of entities(node) linked together in the manner of hirarchy
 
 // Terminologies of tree 
+// Root node
+// Edges
+// Leaf node(External node)
+// Non Leaf node(Internal node)
+// Depth of node
+// Height of node
+// Subtree
+
