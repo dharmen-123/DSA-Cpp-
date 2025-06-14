289c06c6873fa0bd96169f12dca94a40887ae124
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-14 15:55:49 +0530

    Binaary tree

--- Tree.cpp
+++ Tree.cpp
@@ -9,4 +9,16 @@
 // Depth of node
 // Height of node
 // Subtree
+ 
 
+// Binary tree 
+// *Each Node have maximum two child (0,1,2)
+
+// #### TYPES OF BIINARY TREE #######
+// 1 Strict /Proper binary tree : 
+// Each node will contain exactly 2 children 
+//Formula:  no. of leaf node = internal node +1
+
+// 2. Complete Binary tree :
+// All the node are completely filled node followed by left possible 
+ 
