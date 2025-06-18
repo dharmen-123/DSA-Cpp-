70c6dd2de50ee519ebea6c6351037144c77f4bf5
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-18 20:59:36 +0530

    graph

---
+++ Graph.cpp
@@ -0,0 +1,9 @@
+//  ######## GRAPH #################
+// Graph is the nonlinear data Structure with consisting vertices and nodes
+
+// Every node in Graph has unique node 
+// It is the collection of vertices and node G={V,E}
+// It is use in Networking model 
+// Graph may be cyclic or acyclic
+// Every Acyclic Graph is a tree 
+// No. od edges in graph may be less or equal or more than vertices
