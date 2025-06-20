4a8f7206c7252e281b88e0df75e386667eee1590
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-20 15:53:51 +0530

    STL

---
+++ StandardtemplateLibrary/basicsofSTL.cpp
@@ -0,0 +1,8 @@
+// #include"iostream"
+// using namespace std;
+// int main(){
+//    cout<<"Hello"; 
+// }
+
+// Ambiguity
+// Integer and Float type accept float value or int value both that time ambiguity is create 
