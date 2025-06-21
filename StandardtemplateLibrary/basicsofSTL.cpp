98779fcf27ff37aff9564cf326c3852e7c0df089
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-21 15:19:16 +0530

    STL

--- StandardtemplateLibrary/basicsofSTL.cpp
+++ StandardtemplateLibrary/basicsofSTL.cpp
@@ -6,3 +6,15 @@
 
 // Ambiguity
 // Integer and Float type accept float value or int value both that time ambiguity is create 
+
+#include<iostream>
+using namespace std;
+template<class mytype>
+mytype Sum(mytype a){
+    
+    return a+10;
+
+}
+int main(){
+    cout<<Sum(10);
+}
