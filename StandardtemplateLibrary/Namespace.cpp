457b0cf0f2759d9a3ab840d1961e954e077d0bb1
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-23 15:49:59 +0530

    changes

---
+++ StandardtemplateLibrary/Namespace.cpp
@@ -0,0 +1,4 @@
+// namespace is a collection of identifier
+// It removes the Ambiguity 
+// By default nature of Namespace is the public
+// There is no requirement of object for access the variable outside the class
--- StandardtemplateLibrary/basicsofSTL.cpp
+++ StandardtemplateLibrary/basicsofSTL.cpp
@@ -38,6 +38,7 @@
 
 // }
 
+
 // #include"iostream"
 // using namespace std;
 // template<class obj>
@@ -55,30 +56,31 @@
 //    job<float>j(20);
 // }
 
-// #include"iostream"
-// using namespace std;
-// template<class obj1,class obj2>
-// class job{
-//  public:
-//  obj1 v1;
-//  obj2 v2;
+#include"iostream"
+using namespace std;
+template<class obj1,class obj2>
+class job{
+ public:
+ obj1 v1;
+ obj2 v2;
 
-//  job(obj1 val1,obj2 val2){
-//     v1=val1;
-//     v2=val2;
-//     cout<<"Value ="<<v1+v2;
-//  }   
-// };
+ job(obj1 val1,obj2 val2){
+    v1=val1;
+    v2=val2;
+    cout<<"Value ="<<v1+v2;
+ }   
+};
+int main(){
+   job<int, float>j(20,10.3);
+}
+
+// #include<iostream>
+// using namespace std;
+// template<class mytype>
+// void Show(mytype a){
+//    cout<<"This is number :"<<a;
+// }
 // int main(){
-//    job<int, float>j(20,10.3);
+// Show(10);
 // }
 
-#include<iostream>
-using namespace std;
-template<class mytype>
-void Show(mytype a){
-   cout<<"This is number :"<<a;
-}
-int main(){
-Show(10);
-}
