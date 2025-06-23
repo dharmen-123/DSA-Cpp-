65f7c376663fca35bd835585ee90010efafa1f54
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-23 16:33:56 +0530

    commit

---
+++ StandardtemplateLibrary/ArrayofSTL.cpp
@@ -0,0 +1,18 @@
+
+#include<iostream>
+using namespace std;
+#include<array>
+int main(){
+array<int,5>a{2,6,4,5,7};
+array<int,5>a2{10,20,30,40,50};
+
+cout<<"Size of array :"<<a.size()<<"\n";
+for(int i=0;i<a.size();i++){
+    cout<<a.at(i)<<"\t";
+}
+cout<<"value of second array :";
+for(int i=0;i<a2.size();i++){
+    cout<<a2.at(i)<<"\t";
+}
+
+}
