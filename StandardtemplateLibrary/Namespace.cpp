1cf735f81353fe0233a1ffc38507edd5f270c9bf
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-06-23 16:06:46 +0530

    namespace

--- StandardtemplateLibrary/Namespace.cpp
+++ StandardtemplateLibrary/Namespace.cpp
@@ -2,3 +2,37 @@
 // It removes the Ambiguity 
 // By default nature of Namespace is the public
 // There is no requirement of object for access the variable outside the class
+// In namespace the variable is access outside the namepsace using Scope resolution operator
+
+
+// #include<iostream>
+// using namespace std;
+// namespace Hello{
+//     int a=100;
+//     int b=200;
+// }
+// int main(){
+//   cout<<Hello::a;
+// }
+//   ########################################
+
+#include<iostream>
+using namespace std;
+namespace Hello{
+    int a=100;
+    int b=200;
+void multiply(){
+    cout<<"\nMultiply :"<<a*b;
+}
+class Bhopal{
+  public:
+  int x=20;
+    
+}obj;
+}
+int main(){
+  cout<<Hello::a;
+  Hello::multiply();
+  cout<<"\n"<<Hello::obj.x;
+
+}
