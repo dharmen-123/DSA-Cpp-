0f7c1f21937b390c06ec3d25654aad69f4bbce40
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-05 16:20:47 +0530

    commit

--- PractiseQuestion/StringQuestion.cpp
+++ PractiseQuestion/StringQuestion.cpp
@@ -118,6 +118,12 @@
 //      getline(cin,s1);
 //      string s2=s1;
 //      reverse(s1.begin(),s1.end());
+//	    for(int i=0;i<s1.size();i++){
+//		char store=str[i];
+//		str[i]=str[j];
+//		str[j]=store;
+//		j--;
+//	}
 //      if(s1==s2){
 //       cout<<"True";
 //      }
@@ -232,6 +238,36 @@ using namespace std;
 int main(){
 
     string s="cpp is the world Fastest programming language";
+    string b, str;
     
+    for(int i=0;i<s.size();i++){
+        if(s[i]!=' '){
+           b.push_back(s[i]);
+        }
+        else{
+            break;
+        }
+    }
+  cout<<"backup"<<endl;
+  for(int i=0;i<b.size();i++){
+    cout<<b[i];
+  } 
+  for(int i=0;i<s.size();i++){
+
+      if(s[i]!=' '){
+        str.push_back(s[i]);
+        if(i+1==s.size() && str.size()>b.size()){
+            b=str;
+        }
+      }
+      else{
+          if(str.size()>b.size()){
+            b=str;
+          }
+          str.clear();
+      }
+  }
+    
+  cout<<"\nFinal Output :"<<b;
     
 }
