51f08a2473e7412edd4f37a1e9db99c3bd8058bf
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-09 16:01:36 +0530

    Singliy linked list

--- LinearDS/LIST.cpp
+++ LinearDS/LIST.cpp
@@ -1,2 +1,25 @@
 // // ######## Forward list or singly linked list  ##############
+// // It is non contiguous memory allocation 
 
+
+#include<iostream>
+using namespace std;
+#include<forward_list>
+
+int main(){
+ 
+    forward_list<int>f;
+    f.push_front(10);
+    f.push_front(43);
+    f.push_front(25);
+    f.push_front(26);
+
+    for(auto p:f){
+        cout<<p<<"\t";
+    }
+    f.pop_front();
+cout<<"\nAfter the deletion "<<endl;
+    for(auto p:f){
+        cout<<p<<"\t";
+    }
+}
