a6898592dda8f0a969d644b62925f143fa46dd59
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-09 16:33:59 +0530

    commit

--- LinearDS/LIST.cpp
+++ LinearDS/LIST.cpp
@@ -1,10 +1,12 @@
 // // ######## Forward list or singly linked list  ##############
 // // It is non contiguous memory allocation 
+//  It doest not have size() function
 
 
 #include<iostream>
 using namespace std;
-#include<forward_list>
+// #include<forward_list>
+#include<bits/stdc++.h>
 
 int main(){
  
@@ -22,4 +24,31 @@ cout<<"\nAfter the deletion "<<endl;
     for(auto p:f){
         cout<<p<<"\t";
     }
+    cout<<"\nNext list "<<endl;
+    forward_list<int>f2{45,23,76,35,20};
+        for(auto p:f2){
+        cout<<p<<"\t";
+    }
+    f2.sort();
+    cout<<"\nAfter sorting f2:\n";
+      for(auto p:f2){
+        cout<<p<<"\t";
+    }
+    f.sort();
+    f.merge(f2);
+    cout<<"\nAfter merge the f and f2:"<<endl;
+    for(auto p:f){
+        cout<<p<<"\t";
+    }   
+    forward_list<int>f3{65,35,75,86,24};
+    f3.sort();
+     f3.reverse();
+    cout<<"\nf3 sort and reverse data\n";
+    for(auto p:f3){
+        cout<<p<<"\t";
+    }
+//////// size find in list
+ cout<<std::distance(f3.begin(),f3.end());
+ 
+
 }
