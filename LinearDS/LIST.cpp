b733da2a5028fa19db1ab6a807f826bb0d778210
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-09 22:51:25 +0530

    List

--- LinearDS/LIST.cpp
+++ LinearDS/LIST.cpp
@@ -3,52 +3,52 @@
 //  It doest not have size() function
 
 
-#include<iostream>
-using namespace std;
-// #include<forward_list>
-#include<bits/stdc++.h>
+// #include<iostream>
+// using namespace std;
+// // #include<forward_list>
+// #include<bits/stdc++.h>
 
-int main(){
+// int main(){
  
-    forward_list<int>f;
-    f.push_front(10);
-    f.push_front(43);
-    f.push_front(25);
-    f.push_front(26);
+//     forward_list<int>f;
+//     f.push_front(10);
+//     f.push_front(43);
+//     f.push_front(25);
+//     f.push_front(26);
 
-    for(auto p:f){
-        cout<<p<<"\t";
-    }
-    f.pop_front();
-cout<<"\nAfter the deletion "<<endl;
-    for(auto p:f){
-        cout<<p<<"\t";
-    }
-    cout<<"\nNext list "<<endl;
-    forward_list<int>f2{45,23,76,35,20};
-        for(auto p:f2){
-        cout<<p<<"\t";
-    }
-    f2.sort();
-    cout<<"\nAfter sorting f2:\n";
-      for(auto p:f2){
-        cout<<p<<"\t";
-    }
-    f.sort();
-    f.merge(f2);
-    cout<<"\nAfter merge the f and f2:"<<endl;
-    for(auto p:f){
-        cout<<p<<"\t";
-    }   
-    forward_list<int>f3{65,35,75,86,24};
-    f3.sort();
-     f3.reverse();
-    cout<<"\nf3 sort and reverse data\n";
-    for(auto p:f3){
-        cout<<p<<"\t";
-    }
-//////// size find in list
- cout<<std::distance(f3.begin(),f3.end());
- 
+//     for(auto p:f){
+//         cout<<p<<"\t";
+//     }
+//     f.pop_front();
+// cout<<"\nAfter the deletion "<<endl;
+//     for(auto p:f){
+//         cout<<p<<"\t";
+//     }
+//     cout<<"\nNext list "<<endl;
+//     forward_list<int>f2{45,23,76,35,20};
+//         for(auto p:f2){
+//         cout<<p<<"\t";
+//     }
+//     f2.sort();
+//     cout<<"\nAfter sorting f2:\n";
+//       for(auto p:f2){
+//         cout<<p<<"\t";
+//     }
+//     f.sort();
+//     f.merge(f2);
+//     cout<<"\nAfter merge the f and f2:"<<endl;
+//     for(auto p:f){
+//         cout<<p<<"\t";
+//     }   
+//     forward_list<int>f3{65,35,75,86,24};
+//     f3.sort();
+//      f3.reverse();
+//     cout<<"\nf3 sort and reverse data\n";
+//     for(auto p:f3){
+//         cout<<p<<"\t";
+//     }
+// //////// size find in list
+//  cout<<std::distance(f3.begin(),f3.end());
 
-}
+
+// }
