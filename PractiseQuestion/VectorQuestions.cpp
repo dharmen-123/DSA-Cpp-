48a176463dd112dab15a34157257db38d7d74598
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-11 22:34:30 +0530

    commit

--- PractiseQuestion/VectorQuestions.cpp
+++ PractiseQuestion/VectorQuestions.cpp
@@ -125,19 +125,25 @@
 using namespace std;
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
           vector<int>num;
-          int l=m+n;
           for(int i=0;i<m;i++){
-            if(nums1[i]!=0){
+            if(nums1[i]==0){
              num.push_back(nums1[i]);
           }
           }
-          
+          for(int j=0;j<n;j++){
+             num.push_back(nums2[j]);
+          }
+          sort(num.begin(),num.end());
     }
 
 int main(){
-    vector<int>nums1{1,2,3,0,0,0};
-    int m = 3;
-    vector<int>nums2{2,5,6};
-    int  n = 3;
+    // vector<int>nums1{1,2,3,0,0,0};
+    vector<int>nums1{1};
+    // int m = 3;
+    int m = 1;
+    // vector<int>nums2{2,5,6};
+    vector<int>nums2{};
+    // int  n = 3;
+    int  n = 0;
     merge(nums1,m,nums2,n);
 }
