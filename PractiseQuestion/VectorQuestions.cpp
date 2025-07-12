b4b81d458cd59cd05a45fb92c3b540df82a118ac
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-12 13:11:15 +0530

    commit

--- PractiseQuestion/VectorQuestions.cpp
+++ PractiseQuestion/VectorQuestions.cpp
@@ -171,39 +171,46 @@
 //       int k = 2;
 //       rotate(nums,k);
 // }
+ 
+// ############################# Q.       ######################
 
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+// void rotate(vector<int>& nums, int k) {
+//     int n = nums.size();
+//     k = k % n; 
+//     reverse(nums.begin(), nums.end());
+//     reverse(nums.begin(), nums.begin() + k);
+//     reverse(nums.begin() + k, nums.end());
+//     for (int num : nums) {
+//         cout << num << " ";
+//     }
+//     cout << endl;
+// }
 
-void rotate(vector<int>& nums, int k) {
-    int n = nums.size();
-    k = k % n; 
-    reverse(nums.begin(), nums.end());
-    for (int num : nums) {
-        cout << num << " ";
-    }
-    cout<<"\n";
-    reverse(nums.begin(), nums.begin() + k);
-    for (int num : nums) {
-        cout << num << " ";
-    }
-    cout<<"\n";
-    reverse(nums.begin() + k, nums.end());
-    
-    for (int num : nums) {
-        cout << num << " ";
-    }
-    cout << endl;
-}
+// int main() {
+//     // vector<int> nums{1, 2, 3, 4, 5, 6, 7};
+//       vector<int>nums{-1,-100,3,99};
+//     int k = 2;
+//     rotate(nums, k);
+  
+// }
 
-int main() {
-    vector<int> nums{1, 2, 3, 4, 5, 6, 7};
-      // vector<int>nums{-1,-100,3,99};
-    int k = 3;
-    rotate(nums, k);
-    
+////  ########## Q.153. Find Minimum in Rotated Sorted Array 
 
-}
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+//     int findMin(vector<int>& nums) {
+//         sort(nums.begin(),nums.end());
+//         return nums[0];
+//     }
+// int main(){
+//   //  vector<int>nums{3,4,5,1,2};
+//    vector<int>nums{4,5,6,7,0,1,2};
+//   cout<<findMin(nums);
+//   }
+
