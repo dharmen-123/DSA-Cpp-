6dd3acb899edcc6a04d3a784c5b957e3cbf821a7
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-12 20:32:11 +0530

    commit

--- LinearDS/QUEUE.cpp
+++ LinearDS/QUEUE.cpp
@@ -4,21 +4,48 @@
 // methods in queue 
 // push , pop , back , front 
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     queue<int>q;
+//     q.push(10);
+//     q.push(5);
+//     q.push(78);
+//     q.push(23);
+//     q.push(4);
+//     cout<<"last element :"<<q.back()<<endl;
+//     while (!q.empty())
+//     {
+//       cout<<q.front()<<endl;
+//       q.pop();
+//     }
+// }
+
+        //   ########### DEQUE #################
+//   Insertion and Deletion from both side 
+//  It belongs to non contiguous memory 
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    queue<int>q;
-    q.push(10);
-    q.push(5);
-    q.push(78);
-    q.push(23);
-    q.push(4);
-    cout<<"last element :"<<q.back()<<endl;
-    while (!q.empty())
-    {
-      cout<<q.front()<<endl;
-      q.pop();
-    }
+  deque<int>d;
+  d.push_front(2);
+  d.push_back(5);
+  d.push_front(7);
+  d.push_back(9);
+
+  for(auto p:d){
+    cout<<p<<" ";
+  }
+  for(auto &q:d){
+    cout<<++q<<" ";
+  }
+
 }
+
+//##############  auto ########
+// auto bring the copy of the value of array 
