a9ac14c22542d849899716021ef461812484b3c9
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-16 15:44:01 +0530

    commit

--- StandardtemplateLibrary/SET.cpp
+++ StandardtemplateLibrary/SET.cpp
@@ -8,34 +8,35 @@
 // It is use to show the data in sorted order 
 
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
+// #include<iostream>
+// #include<bits/stdc++.h>
+// #include<set>
+// using namespace std;
 
-int main(){
-  set<int>s;
-  s.insert(20);
-  s.insert(75);
-  s.insert(86);
-  s.insert(64);
-  s.insert(24);
-  for(auto a:s){
-    cout<<a<<"\n";
-}
-//  s.clear();
+// int main(){
+//   set<int>s;
+//   s.insert(20);
+//   s.insert(75);
+//   s.insert(86);
+//   s.insert(64);
+//   s.insert(24);
+//   for(auto a:s){
+//     cout<<a<<"\n";
+// }
+// //  s.clear();
+// // s.erase(683);
+// auto p=s.find(43);
+// if(p!=s.end()){
+//     cout<<"found\n";
+// }
+// else{
+//     cout<<"Not found\n";
+// }
+//   for(auto a:s){
+//     cout<<a<<"\n";
+// }
+// }
 
-// s.erase(683);
-auto p=s.find(43);
-if(p!=s.end()){
-    cout<<"found\n";
-}
-else{
-    cout<<"Not found\n";
-}
-  for(auto a:s){
-    cout<<a<<"\n";
-}
-}
 
  ////// ############# SEGMENTATION FALULT ########################### 
  ////// ############# SEGMENTATION FALULT ########################### 
@@ -46,3 +47,29 @@ else{
 // due to handle  this  fault use pointer 
 // find is iterater it return the address thats why we auto is used
 // auto p =s.find(data);
+
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<unordered_set>
+using namespace std;
+
+int main(){
+   unordered_set<string>s1{"pen","pencil","eraser","alphabet","watch"};
+    //   set<string>s1{"pen","pencil","eraser","alphabet","watch"};      
+   for(auto a:s1){
+     cout<<a<<"\n";
+   }
+}
+
+
+
+
+// erase() delete all same data
+// when erase is used with find it delete the first value from data 
+// COMPLEX DATA TYPE is pair and tuple  IMP IMP 
+
+
+//    ######## Multiset #########
+// multiset is used to allow the duplicacy and maintain the order 
+
