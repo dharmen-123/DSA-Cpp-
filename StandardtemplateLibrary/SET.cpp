b97b4751b22bb65750f7875327a3c47062149a9a
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-16 15:10:02 +0530

    Set methods

--- StandardtemplateLibrary/SET.cpp
+++ StandardtemplateLibrary/SET.cpp
@@ -19,7 +19,30 @@ int main(){
   s.insert(86);
   s.insert(64);
   s.insert(24);
-for(auto a:s){
+  for(auto a:s){
+    cout<<a<<"\n";
+}
+//  s.clear();
+
+// s.erase(683);
+auto p=s.find(43);
+if(p!=s.end()){
+    cout<<"found\n";
+}
+else{
+    cout<<"Not found\n";
+}
+  for(auto a:s){
     cout<<a<<"\n";
 }
 }
+
+ ////// ############# SEGMENTATION FALULT ########################### 
+ ////// ############# SEGMENTATION FALULT ########################### 
+
+// what is segmentation fault in cpp 
+// When we delete a data which is not exist in set 
+// find and erase are not give the message in return it is segmentation fault 
+// due to handle  this  fault use pointer 
+// find is iterater it return the address thats why we auto is used
+// auto p =s.find(data);
