f2c7612ae4e7a2b49c309ffe64e93cd243d03f1c
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-17 22:38:07 +0530

    commit

--- PractiseQuestion/Questions.cpp
+++ PractiseQuestion/Questions.cpp
@@ -344,36 +344,37 @@
 //     cout<<maxSubArray(nums);
 // }
 
+// ############################# Q. 8 ######################### 
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int myAtoi(string s) {
-   int a = 0;
-    int sign = 1;
-    bool started = false;
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int myAtoi(string s) {
+//    int a = 0;
+//     int sign = 1;
+//     bool started = false;
 
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] == '-' && !started) {
-            sign = -1;
-            started = true;
-        } else if (isdigit(s[i])) {
-            a = a * 10 + (s[i] - '0');
-            started = true;
-        } else if (started) {
-            break; 
-        }
-         else if(s[i] == ' '){
-            continue;
-         }
-        else{
-            return 0;
-        }
-    }
-    return a * sign;
+//     for (int i = 0; i < s.size(); i++) {
+//         if (s[i] == '-' && !started) {
+//             sign = -1;
+//             started = true;
+//         } else if (isdigit(s[i])) {
+//             a = a * 10 + (s[i] - '0');
+//             started = true;
+//         } else if (started) {
+//             break; 
+//         }
+//          else if(s[i] == ' '){
+//             continue;
+//          }
+//         else{
+//             return 0;
+//         }
+//     }
+//     return a * sign;
 
-}
-int main(){
-    string s="  -042";
-    cout<<myAtoi(s);
-}
+// }
+// int main(){
+//     string s="  -042";
+//     cout<<myAtoi(s);
+// }
