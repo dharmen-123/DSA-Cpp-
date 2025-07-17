5bc9fe9e285350006888f0ae473696561e1470a6
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-17 21:40:16 +0530

    commit

--- PractiseQuestion/Questions.cpp
+++ PractiseQuestion/Questions.cpp
@@ -322,8 +322,11 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
-
+int maxSubArray(vector<int>& nums) {
+ 
+    
+}
 int main(){
-
+    vector<int>nums{-2,1,-3,4,-1,2,1,-5,4};
+    cout<<maxSubArray(nums);
 }
