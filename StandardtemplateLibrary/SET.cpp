b8c24441d6b04ff6fb61765f12f021e91d176ee3
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-18 22:57:16 +0530

    commit set

--- StandardtemplateLibrary/SET.cpp
+++ StandardtemplateLibrary/SET.cpp
@@ -72,3 +72,32 @@
 
 //    ######## Multiset #########
 // multiset is used to allow the duplicacy and maintain the order 
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<set>
+using namespace std;
+
+int main(){
+  set<int>s;
+  s.insert(20);
+  s.insert(75);
+  s.insert(86);
+  s.insert(64);
+  s.insert(24);
+  for(auto a:s){
+    cout<<a<<"\n";
+}
+//  s.clear();
+// s.erase(683);
+auto p=s.find(43);
+if(p!=s.end()){
+    cout<<"found\n";
+}
+else{
+    cout<<"Not found\n";
+}
+  for(auto a:s){
+    cout<<a<<"\n";
+}
+}
