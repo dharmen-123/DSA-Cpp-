883cf6392ce97dc58431492975a537fe8955d700
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-18 22:55:45 +0530

    commit

--- PractiseQuestion/Questions.cpp
+++ PractiseQuestion/Questions.cpp
@@ -380,32 +380,32 @@
 
 
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
- void backtrack(vector<string>& result, string current, int open, int close, int n) {
-        if (current.length() == 2 * n) {
-            result.push_back(current);
-            return;
-        }
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+//  void backtrack(vector<string>& result, string current, int open, int close, int n) {
+//         if (current.length() == 2 * n) {
+//             result.push_back(current);
+//             return;
+//         }
 
-        if (open < n) {
-            backtrack(result, current + '(', open + 1, close, n);
-        }
+//         if (open < n) {
+//             backtrack(result, current + '(', open + 1, close, n);
+//         }
 
-        if (close < open) {
-            backtrack(result, current + ')', open, close + 1, n);
-        }
-    }
+//         if (close < open) {
+//             backtrack(result, current + ')', open, close + 1, n);
+//         }
+//     }
 
-    vector<string>generateParenthesis(int n) {
-        vector<string> result;
-        backtrack(result, "", 0, 0, n);
-        return result;
-    }
+//     vector<string>generateParenthesis(int n) {
+//         vector<string> result;
+//         backtrack(result, "", 0, 0, n);
+//         return result;
+//     }
 
 
-int main(){
-    int n=3;
-    generateParenthesis(n);
-}
+// int main(){
+//     int n=3;
+//     generateParenthesis(n);
+// }
