d42f93d3c8ef38f6bd902c4aff491d11262bc7c7
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-19 23:08:45 +0530

    commit

--- PractiseQuestion/StringQuestion.cpp
+++ PractiseQuestion/StringQuestion.cpp
@@ -381,27 +381,28 @@
 //    cout<<orderlyQueue(s,k);
 // }
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int romanToInt(string s) {
-        unordered_map<char, int>mp{
-          {'I', 1},
-          {'V', 5},
-          {'X', 10},
-          {'L', 50},
-          {'C', 100},
-          {'D', 500},
-          {'M', 1000}
-        };
-        int sum=0;
-        for(int i=0;i<s.size();i++){
-          sum=sum+mp[s[i]];
-        }
-     return sum;
-}
-int main(){
-      //  string s="LVIII";
-       string s="MCMXCIV";
-       cout<<romanToInt(s);
-}
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int romanToInt(string s) {
+//         unordered_map<char, int>mp{
+//           {'I', 1},
+//           {'V', 5},
+//           {'X', 10},
+//           {'L', 50},
+//           {'C', 100},
+//           {'D', 500},
+//           {'M', 1000}
+//         };
+//         int sum=0;
+//         for(int i=0;i<s.size();i++){
+//           sum=sum+mp[s[i]];
+//         }
+//      return sum;
+// }
+// int main(){
+//       //  string s="LVIII";
+//        string s="MCMXCIV";
+//        cout<<romanToInt(s);
+// }
