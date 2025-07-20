988e3a9bf184d4b5ff19a1e900d1c1d20f0c0281
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-20 11:23:37 +0530

    Integer to roman

--- PractiseQuestion/StringQuestion.cpp
+++ PractiseQuestion/StringQuestion.cpp
@@ -435,36 +435,30 @@
 //        cout<<romanToInt(s);
 // }
 
+// // ################  Q.14 ##################
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-string intToRoman(int num) {
-    // int values[] = {1000, 900, 500, 400, 100, 90,
-    //                  50,  40,  10,   9,   5,  4, 1};
-    // string symbols[] = {"M", "CM", "D", "CD", "C", "XC",
-    //                   "L", "XL", "X", "IX", "V", "IV", "I"};
-    //    string r="";
-    //    for(int i=0;i<13;i++){
-    //         while(num>=values[i]){
-    //           r+=symbols[i];
-    //           num-=values[i];
-    //         }
-    //    }     
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
 
-    //    return r;   
-    map<int , string , greater<int>>mp{
-          {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
-        {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
-        {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"}, {1, "I"}
-    };
-    string r="";
-    for(auto i=mp.begin();i!=mp.end();i++){
-      
-    }
+//   string intToRoman(int num) {    
+//     map<int ,string,greater<int>>mp{
+//           {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+//         {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+//         {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"}, {1, "I"}
+//     };
+//     string r="";
+//     for(auto i=mp.begin();i!=mp.end();i++){
+//          while(num>=i->first){
+//             r+=i->second;
+//             num-=i->first;
 
-}
-int main(){
-  int num = 3749;
-  cout<<intToRoman(num);
-}
+//          }
+//     }
+//       return r;
+// }
+// int main(){
+//   int num = 3749;
+//   cout<<intToRoman(num);
+
+// }
