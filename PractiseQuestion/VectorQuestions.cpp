b299e44471b975f60a49696623cc4fc2df381c82
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-22 19:11:15 +0530

    commit

--- PractiseQuestion/VectorQuestions.cpp
+++ PractiseQuestion/VectorQuestions.cpp
@@ -449,30 +449,28 @@ using namespace std;
 
 vector<vector<int>> fourSum(vector<int>& nums , int target) {
     vector<vector<int>> v;
-    sort(nums.begin(), nums.end());
-
-    int n = nums.size();
-    for (int i = 0; i < n - 3; i++) {
-        if (i > 0 && nums[i] == nums[i - 1]) continue;
-        for (int j = i + 1; j < n - 2; j++) {
-            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-            int left = j + 1, right = n - 1;
-            while (left < right) {
-                long long total = (long long)nums[i] + nums[j] + nums[left] + nums[right];
-                if (total == target) {
-                    v.push_back({nums[i], nums[j], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-                    left++;
-                    right--;
-                } else if (total < target) {
-                    left++;
-                } else {
-                    right--;
-                }
+    sort(nums.begin(), nums.end());  
+    for (int i = 0; i < nums.size() - 3; i++) {
+        if (i > 0 && nums[i] == nums[i - 1]) continue;  
+    for (int j = i+1; j < nums.size() - 2; j++) {
+        if (j > i+1 && nums[j] == nums[j - 1]) continue;  
+        int left = j + 1;
+        int right = nums.size() - 1;
+        while (left < right) {
+            long long total =(long long)nums[i] + nums[j] + nums[left]+nums[right];
+            if (total == target) {
+                v.push_back({nums[i], nums[j], nums[left],nums[right]});
+                while (left < right && nums[left] == nums[left + 1]) left++;  // Skip duplicates
+                while (left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
+            } else if (total < target) {
+                left++;
+            } else {
+                right--;
             }
         }
+    }
     }
     return v;
 }
