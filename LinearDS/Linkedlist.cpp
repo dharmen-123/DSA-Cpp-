b4ecf2d6e9e92ee7ec0b91303b08fa2e0f100f56
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-27 19:56:27 +0530

    commit

--- LinearDS/Linkedlist.cpp
+++ LinearDS/Linkedlist.cpp
@@ -321,23 +321,3 @@ int main(){
    show(head);
 }
 
-// // // ##################  Q. 19 #######################
-
-// #include<iostream>
-// #include<bits/stdc++.h>
-// using namespace std;
-// struct ListNode{
-//     int val;
-//     ListNode *next;
-//     ListNode() : val(0), next(nullptr) {}
-//     ListNode(int x) : val(x), next(nullptr) {}
-//     ListNode(int x, ListNode *next) : val(x), next(next) {}
-
-// };
-// ListNode* removeNthFromEnd(ListNode* head, int n) {
-                   
-
-// }
-// int main(){
-
-// }
