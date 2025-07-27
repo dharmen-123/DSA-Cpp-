6fabb4c5e9448b7943789b5b48855fa44257b168
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-07-27 10:53:12 +0530

    commit

--- LinearDS/Linkedlist.cpp
+++ LinearDS/Linkedlist.cpp
@@ -127,6 +127,139 @@
 
 // // // Delete from the Kth position of the list ######
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// struct Node{
+//     int data;
+//     Node* next;
+
+//     Node(int data1,Node* next1){
+//          data=data1;
+//          next=next1;
+//     }
+//     Node(int data1){
+//     data=data1;
+//     next=NULL;
+//    }
+// };
+// Node* ArrtoLL(vector<int>& arr){
+//      Node* head=new Node(arr[0]);
+//      Node* move=head;
+//      for(int i=1;i<arr.size();i++){
+//         Node* temp = new Node(arr[i]);
+//         move->next=temp;
+//         move=temp;
+//      }
+//      return head;
+// }
+// void show(Node* head){
+//      while(head!=NULL){
+//         cout<<head->data<<" ";
+//          head=head->next;
+//      }
+//      cout<<endl;
+// }
+
+// Node* deleteKth(Node* head, int k){
+//      if(head==NULL) return head;
+//     if (k==1){
+//        Node* temp=head;
+//        head=head->next;
+//        free(temp);
+//      return head;
+//     }
+//     int cnt=0;
+//     Node* temp=head;
+//     Node* prev=NULL;
+//     while(temp!=NULL){
+//         cnt++;
+//         if(cnt==k){
+//             prev->next=prev->next->next;
+//             delete temp;
+//             break;
+//         }
+//         prev=temp;
+//         temp = temp->next;
+//     }
+//     return head;
+// }
+// int main(){
+//    vector<int>arr{2,4,1,6,8};
+//    Node* head =ArrtoLL(arr);
+//    show(head);
+//    head=deleteKth(head,3);
+//    show(head);
+
+// }
+
+// // //  ############### Deletion from the given value ################
+
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std;
+// struct Node{
+//     int data;
+//     Node* next;
+
+//     Node(int data1,Node* next1){
+//          data=data1;
+//          next=next1;
+//     }
+//     Node(int data1){
+//     data=data1;
+//     next=NULL;
+//    }
+// };
+// Node* ArrtoLL(vector<int>& arr){
+//      Node* head=new Node(arr[0]);
+//      Node* move=head;
+//      for(int i=1;i<arr.size();i++){
+//         Node* temp = new Node(arr[i]);
+//         move->next=temp;
+//         move=temp;
+//      }
+//      return head;
+// }
+// void show(Node* head){
+//      while(head!=NULL){
+//         cout<<head->data<<" ";
+//          head=head->next;
+//      }
+//      cout<<endl;
+// }
+
+// Node* deleteElement(Node* head, int val){
+//      if(head==NULL) return head;
+//     if (head->data==val){
+//        Node* temp=head;
+//        head=head->next;
+//        delete temp;
+//      return head;
+//     }
+//     Node* temp=head;
+//     Node* prev=NULL;
+//     while(temp!=NULL){
+//         if(temp->data==val){
+//             prev->next=temp->next;
+//             delete temp;
+//            break;
+//         }
+//         prev=temp;
+//         temp = temp->next;
+//     }
+//     return head;
+// }
+// int main(){
+//    vector<int>arr{2,4,1,6,8,4};
+//    Node* head =ArrtoLL(arr);
+//    show(head);
+//    head=deleteElement(head,4);
+//    show(head);
+// }
+
+// // //  ############### Deletion from the all duplicate of given value ################
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
@@ -161,14 +294,32 @@ void show(Node* head){
      cout<<endl;
 }
 
-
+Node* deleteElement(Node* head, int val){
+      while(head!=NULL && head->data==val){
+        Node* temp=head;
+        head=head->next;
+        delete temp;
+      }
+      Node* curr=head;
+      while(curr!=NULL && curr->next!=NULL){
+          if(curr->next->data==val){
+            Node* temp= curr->next;
+            curr->next=curr->next->next;
+            delete temp;
+          }
+          else{
+            curr=curr->next;
+          }
+      }
+    return head;
+}
 int main(){
-   vector<int>arr{2,4,1,6};
+   vector<int>arr{2,4,1,6,8,4};
    Node* head =ArrtoLL(arr);
    show(head);
-
+   head=deleteElement(head,4);
+   show(head);
 }
-
 
 // // // ##################  Q. 19 #######################
 
