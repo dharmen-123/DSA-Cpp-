9859cca8880d22aa327884710c8ed39b8053f6c1
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-09 23:02:30 +0530

    changes

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -530,7 +530,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 int maxProfit(vector<int>& prices) {
-        
+         int min_price = INT_MAX;
+    int max_profit = 0;
+
+    for (int price : prices) {
+        if (price < min_price) {
+            min_price = price;
+        } else {
+            max_profit = max(max_profit, price - min_price);
+        }
+    }
+
+    return max_profit;
+
 }
 int main(){
  vector<int>prices{7,1,5,3,6,4};
