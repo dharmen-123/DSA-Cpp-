e9f2d269f3dc9022f8210a86770a71096b2f484b
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-09 23:01:06 +0530

    changes

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -529,10 +529,12 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
-
+int maxProfit(vector<int>& prices) {
+        
+}
 int main(){
-
+ vector<int>prices{7,1,5,3,6,4};
+ cout<<maxProfit(prices);
 }
 
 
