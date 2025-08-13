28b838bf304744e06be7320b676a3bd823f8c1d7
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-13 23:35:07 +0530

    find duplicacte

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -585,8 +585,16 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-
-
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]==nums[i+1]){
+                return nums[i];
+            }
+        }
+    return 0;
+    }
 int main(){
-
+   vector<int>nums{1,3,4,2,2};
+   cout<<findDuplicate(nums);
 }
