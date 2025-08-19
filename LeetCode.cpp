71397943efe8c06a8943e845dd38d90d0597f8ad
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-08-19 17:19:26 +0530

    commit

--- LeetCode.cpp
+++ LeetCode.cpp
@@ -633,12 +633,3 @@
 
 // // // ############# Q.    ###########################
 
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-
-
-int main(){
-   vector<int>nums{};
-   cout<<endl;
-}
