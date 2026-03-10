9d4313cf827d3b1f5df3fa397674bc41da79ba59
dharmen-123 <dharmendrachilhate11@gmail.com>
2026-03-10 22:22:11 +0530

    commit

--- Practice.cpp
+++ Practice.cpp
@@ -333,4 +333,25 @@
 
 
 // // // //  Two Sum LeetCode Question // // // //
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+    vector<int> twoSum(vector<int>& nums, int target) {
+       vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    result.push_back(i);
+                    result.push_back(j);
+                    return result; // return immediately once found
+                }
+            }
+        }
+        return result; // empty if no solution
+
+    }
+int main(){
+     
+
+}
 
